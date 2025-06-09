9891d554ba30d3f07bc3bdb74e24e383a81e7adb
aqxorus <76942044+Aqxorus@users.noreply.github.com>
2025-06-09 22:56:49 +1200

    refactor: remove legacy file loading and event handling
    - Deleted the fileLoader and eventHandler modules as they were no longer in use.
    - Removed the ping command and its associated cooldown validation logic.
    - Cleaned up the ready event initialization code.
    - Updated the main index file to integrate the new bot structure and command/event loading.
    - Introduced a new bot implementation using @discordeno/bot with improved command handling.
    - Added new command registration and interaction handling logic.
    - Implemented permission calculation for commands.
    - Updated TypeScript configuration for better compatibility and performance.
    - Added a new configuration file to manage environment variables.
    - Introduced utility functions for loading commands and updating application commands.

---
+++ src/events/ready.ts
@@ -0,0 +1,10 @@
+import { bot } from '../bot.js';
+
+bot.events.ready = ({ user, shardId }) => {
+  if (shardId === bot.gateway.lastShardId) {
+    // All shards are ready
+    bot.logger.info(
+      `Successfully connected to the gateway as ${user.username}#${user.discriminator}`
+    );
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,14 @@
-import 'dotenv/config';
-import { Client, Partials, GatewayIntentBits, Collection } from 'discord.js';
-import path from 'path';
+import 'dotenv/config'
 
-declare module 'discord.js' {
-  interface Client {
-    events: any;
-  }
-}
+import { bot } from './bot.js'
+import importDirectory from './utils/loader.js'
 
-const { Guilds, GuildMembers, GuildPresences } = GatewayIntentBits;
-const { User, Message, GuildMember, ThreadMember } = Partials;
+bot.logger.info('Starting bot...')
 
-const client = new Client({
-  intents: [Guilds, GuildMembers, GuildPresences],
-  partials: [User, Message, GuildMember, ThreadMember],
-});
+bot.logger.info('Loading commands...')
+await importDirectory('./dist/commands')
 
-client.events = new Collection();
+bot.logger.info('Loading events...')
+await importDirectory('./dist/events')
 
-// new CommandKit({
-//   client,
-//   commandsPath: path.join(__dirname, 'commands'),
-//   eventsPath: path.join(__dirname, 'events'),
-//   validationsPath: path.join(__dirname, 'validations'),
-//   devGuildIds: ['664637118265688074'],
-//   devUserIds: ['598624275083034654'],
-//   // devRoleIds: ['DEV_ROLE_ID_1', 'DEV_ROLE_ID_2'],
-//   // skipBuiltInValidations: true,
-//   bulkRegister: true,
-// });
-
-let token;
-
-switch (process.env.DEBUG_MODE) {
-  case '0':
-    token = process.env.TOKEN;
-
-    break;
-
-  case '1':
-    token = process.env.TEST_TOKEN;
-
-    break;
-}
-
-client.login(token);
+await bot.start()
