30a21026979c24f074e64fd067731841413ff6d6
aqxorus <76942044+Aqxorus@users.noreply.github.com>
2025-06-19 12:43:23 +1200

    refactor: remove old bot structure and implement CustomClient
    - Deleted bot.ts, commands.ts, and related command files to streamline the bot's architecture.
    - Introduced CustomClient class to encapsulate client logic and configuration handling.
    - Removed outdated command registration and event handling methods.
    - Added interfaces for configuration and custom client structure.
    - Updated the initialization process to utilize the new CustomClient class.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import 'dotenv/config'
+import CustomClient from './classes/CustomClient';
 
-import { bot } from './bot.js'
-import importDirectory from './utils/loader.js'
-
-bot.logger.info('Starting bot...')
-
-bot.logger.info('Loading commands...')
-await importDirectory('./dist/commands')
-
-bot.logger.info('Loading events...')
-await importDirectory('./dist/events')
-
-await bot.start()
+new CustomClient().Init();
---
+++ src/interfaces/IConfig.ts
@@ -0,0 +1,10 @@
+export default interface IConfig {
+  token: string;
+  token1: string;
+  apiKey: string;
+  heartbeat: string;
+  baseURL: string;
+  mongoUri: string;
+  defaultCooldown: number;
+  debugMode: number;
+}
